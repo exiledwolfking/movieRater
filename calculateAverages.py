5f2ac04c60ab0eec4041a414a4ae6a3f957e81d8
Kyle Wahl <kylebwahl@gmail.com>
2018-05-13 18:17:52 -0500

    added tv show averages

--- calculateAverages.py
+++ calculateAverages.py
@@ -29,20 +29,56 @@ try:
 
     averages = []
     for avg in movieResults:
-        print(avg)
         Average(
             title=avg['_id'],
             average=avg['average']
         ).save()
         
     # calculate tv show averages and save
-    tvReviews = Review.objects.raw({'season': { '$exists': True}, 'episode': { '$exists': True}})
-    tvResults = tvReviews.aggregate(
-        { '$group': { '_id': '$phone', '$project': '$title'}})
+    tvReviews = Review.objects.raw({'season': { '$ne': None}, 'episode': { '$ne': None}})
+    tvResultsPerUser = tvReviews.aggregate(
+        {
+            '$group': {
+                '_id': {
+                    'phone': '$phone',
+                    'title': '$title'
+                },
+                'userAvg': {
+                    '$avg': '$rating'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'phone': '$_id.phone',
+                'title': '$_id.title',
+                'userAvg': True
+            }
+        }, {
+            '$group': {
+                '_id': {
+                    'title': '$title'
+                },
+                'totalAvg': {
+                    '$avg': '$userAvg'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'title': '$_id.title',
+                'totalAvg': True
+            }
+        }
+    )
 
     averages = []
-    for avg in tvResults:
+    for avg in tvResultsPerUser:
         print(avg)
+        Average(
+            title=avg['title'],
+            average=avg['totalAvg']
+        ).save()
 
 except Exception as e:
     rootLogger.error('calculateAverages ' + str(e))
